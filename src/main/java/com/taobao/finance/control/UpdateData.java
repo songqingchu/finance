08233e3fc6019b1af5c374bd6eb8e918b2ecd62c
songqingchu <422895120@qq.com>
2015-01-28 21:24:51 +0800

    t

--- src/main/java/com/taobao/finance/control/UpdateData.java
+++ src/main/java/com/taobao/finance/control/UpdateData.java
@@ -4,6 +4,5 @@ import com.taobao.finance.base.Hisdata_Base;
 public class UpdateData {
 	public static void main(String args[]){
 		Hisdata_Base.updateDataHistoryAll();
-	//	Hisdata_Base.updateDataHistoryDelta();
 	}
 }
